f3759fe044c229e9e9dc416efbe4884abb80e763
Cola <cola20230223@gmail.com>
2023-03-23 14:48:50 +0800

    测试 ZsxqApi 检索问题和回答问题 OpenAIApi 的回答问题

--- chatbot-api-interfaces/src/test/java/rh/cola/api/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/rh/cola/api/ApiTest.java
@@ -1,6 +1,7 @@
 package rh.cola.api;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -11,6 +12,10 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 
@@ -20,29 +25,33 @@ import java.io.IOException;
  * @Time: 2023/3/22 22:38
  * @Description: ApiTest
  */
+@SpringBootTest
+@RunWith(SpringRunner.class)
 @Slf4j
 public class ApiTest {
 
+    @Value("${chatbot-api.group01.cookie}")
+    private String COOKIE;
+    @Value("${chatbot-api.group01.openAiKey}")
+    private String OPEN_API_KEY;
+    @Value("${Cola.PROXY_HOST}")
+    private String PROXY_HOST;
+    @Value("${Cola.PROXY_PORT}")
+    private Integer PROXY_PORT;
+
+
+    /**
+     * 检索待回答问题
+     *
+     * @throws IOException IOException
+     */
     @Test
     public void query_unanswered_questions() throws IOException {
         CloseableHttpClient httpClient = HttpClientBuilder.create().build();
 
         HttpGet get =
                 new HttpGet("https://api.zsxq.com/v2/groups/88885828885512/topics?scope=unanswered_questions&count=20");
-        get.addHeader("cookie",
-                "zsxqsessionid=97c03173301c277ea1eac84c06b74c52; " +
-                        "abtest_env=product; sajssdk_2015_cross_new_user=1; " +
-                        "zsxq_access_token=CE710491-BD36-C8E6-5B0C-F3A4C539579A_A65ABE64B14CA91F; " +
-                        "sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2218" +
-                        "709cdfbdc4a-0f45a898e2875a-7a545470-921600-18709cdfbdd7c3" +
-                        "%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%7D%2C%22" +
-                        "%24device_id%22%3A%2218709cdfbdc4a-0f45a898e2875a-7a545470-921600" +
-                        "-18709cdfbdd7c3%22%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfYW5vbn" +
-                        "ltb3VzX2lkIjoiMTg3MDljZGZiZGM0YS0wZjQ1YTg5OGUyODc1YS03YTU0NTQ3MC" +
-                        "05MjE2MDAtMTg3MDljZGZiZGQ3YzMiLCIkaWRlbnRpdHlfY29va2llX2lkIjoiM" +
-                        "Tg3MDlkMTcyNGYxZDEtMGY4YzMzZjhmYTA3Yjk4LTdhNTQ1NDcwLTkyMTYwMC0" +
-                        "xODcwOWQxNzI1MDg0OSJ9%22%2C%22history_login_id" +
-                        "%22%3A%7B%22name%22%3A%22%22%2C%22value%22%3A%22%22%7D%7D");
+        get.addHeader("cookie", COOKIE);
         get.addHeader("Content-Type", "application/json;charset=utf8");
 
         CloseableHttpResponse response = httpClient.execute(get);
@@ -55,31 +64,24 @@ public class ApiTest {
 
     }
 
+    /**
+     * 回答问题
+     *
+     * @throws IOException IOException
+     */
     @Test
     public void answer() throws IOException {
         CloseableHttpClient httpClient = HttpClientBuilder.create().build();
 
         HttpPost post =
                 new HttpPost("https://api.zsxq.com/v2/topics/88885828885512/answer");
-        post.addHeader("cookie",
-                "zsxqsessionid=97c03173301c277ea1eac84c06b74c52; " +
-                        "abtest_env=product; sajssdk_2015_cross_new_user=1; " +
-                        "zsxq_access_token=CE710491-BD36-C8E6-5B0C-F3A4C539579A_A65ABE64B14CA91F; " +
-                        "sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2218" +
-                        "709cdfbdc4a-0f45a898e2875a-7a545470-921600-18709cdfbdd7c3" +
-                        "%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%7D%2C%22" +
-                        "%24device_id%22%3A%2218709cdfbdc4a-0f45a898e2875a-7a545470-921600" +
-                        "-18709cdfbdd7c3%22%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfYW5vbn" +
-                        "ltb3VzX2lkIjoiMTg3MDljZGZiZGM0YS0wZjQ1YTg5OGUyODc1YS03YTU0NTQ3MC" +
-                        "05MjE2MDAtMTg3MDljZGZiZGQ3YzMiLCIkaWRlbnRpdHlfY29va2llX2lkIjoiM" +
-                        "Tg3MDlkMTcyNGYxZDEtMGY4YzMzZjhmYTA3Yjk4LTdhNTQ1NDcwLTkyMTYwMC0" +
-                        "xODcwOWQxNzI1MDg0OSJ9%22%2C%22history_login_id" +
-                        "%22%3A%7B%22name%22%3A%22%22%2C%22value%22%3A%22%22%7D%7D");
+        post.addHeader("cookie", COOKIE);
         post.addHeader("Content-Type", "application/json;charset=utf8");
+        post.addHeader("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36");
 
         String paramJson = "{\n" +
                 "  \"req_data\": {\n" +
-                "    \"text\": \"自己去百度！\\n\",\n" +
+                "    \"text\": \"ZsxqApiTest \\n\",\n" +
                 "    \"image_ids\": [],\n" +
                 "    \"silenced\": false\n" +
                 "  }\n" +
@@ -89,6 +91,40 @@ public class ApiTest {
         post.setEntity(stringEntity);
 
         CloseableHttpResponse response = httpClient.execute(post);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String result = EntityUtils.toString(response.getEntity());
+            log.info("result = {}", result);
+        } else {
+            log.error("code:{}", response.getStatusLine().getStatusCode());
+        }
+    }
+
+    /**
+     * 使用 GPT 进行查找问题答案
+     *
+     * @throws IOException IOException
+     */
+    @Test
+    public void answerByOpenAi() throws IOException {
+        /*创建代理对象*/
+        HttpHost proxyHost = new HttpHost(PROXY_HOST, PROXY_PORT);
+        /*创建客户端*/
+        CloseableHttpClient httpClient = HttpClientBuilder.create().setProxy(proxyHost).build();
+        /*创建 POST 请求*/
+        HttpPost post = new HttpPost("https://api.openai.com/v1/completions");
+        post.addHeader("Content-Type", "application/json");
+        post.addHeader("Authorization", "Bearer " + OPEN_API_KEY);
+
+        String question = "say Hello";
+        
+        String paramJson = "{\"model\": \"text-davinci-003\", \"prompt\": \"" + question + "\", \"temperature\": 0, \"max_tokens\": 1024}";
+
+        StringEntity stringEntity = new StringEntity(paramJson, ContentType.create("text/json", "UTF-8"));
+        post.setEntity(stringEntity);
+
+        /*发起请求*/
+        CloseableHttpResponse response = httpClient.execute(post);
+
         if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
             String result = EntityUtils.toString(response.getEntity());
             log.info("result = {}", result);
